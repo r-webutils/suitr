89a0b5c7216aeb88caa9ae4c4e844f65d147c2f4
Gregor de Cillia <de.cillia.gregor@gmail.com>
2019-01-15 19:54:53 +0100

    Add CI tools

--- R/A_unicode_html.R
+++ R/A_unicode_html.R
@@ -3,7 +3,7 @@
 #' @param x a string to be wrapped
 #' @param size the font-size to use
 #' @param style a sting appended to the style attribute of the resulting `span`.
-#' @importFrom htmltools span browsable
+#' @importFrom htmltools span browsable HTML
 #' @export
 unicode_html <- function(x, size = 172, style = "") {
   htmltools::span(x, style = paste0("font-size: ", size, "px;", style)) %>%
